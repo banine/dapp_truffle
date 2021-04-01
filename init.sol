bbde66673ececa23935e6e816272913401977a3a
banine <ba_nine@live.com>
2021-04-01 17:08:34 +0800

    Update init.sol

--- init.sol
+++ init.sol
@@ -1,2 +1,8 @@
-pragma solidity ^0.8.3
+pragma solidity ^0.8.3;
+//"SPDK-License-Identifier:LGPL-3.0-or-later";
 
+contract banineHelloWorld {
+    function getString() public pure returns (string memory){
+        return("Welcome to my Smart Contract");
+    }
+}
